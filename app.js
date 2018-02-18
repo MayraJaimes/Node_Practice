6010bb782a1e720eb64e76a12d615c0bfc7fa7a3
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-18 11:32:52 -0600

    adding if statements that will display a different message when they are written in the terminal command line

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const _ = require('lodash');
 
 const notes = require ('./notes.js');
 
+var command = process.argv[2];
+console.log("command", command);
+
+if (command === "add") {
+    console.log("adding new note");
+} 
+else if (command === "list") {
+    console.log("listing all notes");  
+} 
+else if (command === "read"){
+    console.log("reading notes");
+}
+else if (command ==="fetch"){
+    console.log("fetch new note");
+}
+else {
+    console.log("commnad not recognized");
+}
