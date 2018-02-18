41fe9eaf11c5a3ee2181476d5488706989eb4676
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-18 12:25:40 -0600

    added functions to the commands

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ console.log("starting app");
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require ('./notes.js');
 
+const argv = yargs.argv;
+
 var command = process.argv[2];
 console.log("command", command);
+console.log("yargs", argv);
+
 
 if (command === "add") {
-    console.log("adding new note");
+    notes.addNote(argv.title, argv.body);
 } 
 else if (command === "list") {
-    console.log("listing all notes");  
+    notes.getAll();
 } 
 else if (command === "read"){
-    console.log("reading notes");
+    notes.getNote(argv.title);
 }
-else if (command ==="fetch"){
-    console.log("fetch new note");
+else if (command ==="remove"){
+    notes.removeNote(argv.title);    
 }
 else {
     console.log("commnad not recognized");
