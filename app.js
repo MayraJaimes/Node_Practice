912415feee07652192e3e045fa45ad6a7cb37ac8
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-18 11:25:18 -0600

    cleaned the documents to start adding user input in our files

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 console.log("starting app");
 
 const fs = require('fs');
-const os = require ('os');
 const _ = require('lodash');
+
 const notes = require ('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString("Mayra"));
-
-// var filteredArray = _.uniq(["Mayra", 1, "Mayra", 1, 2, 3, 4]);
-// console.log(filteredArray);
-
-// var res = notes.addNote();
-// console.log(res);
-
-// var add = notes.add(2, 4);
-// console.log(add);
-
-// var user = os.userInfo();
-// fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}.`)
